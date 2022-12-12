8adaef6fee6c40e0a0195c7da44a94f9e92bc8c7
unknown <tobirijah@gmail.com>
2022-12-12 03:21:07 -0800

    some changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from "./context";
-import Contract from "./contract/Contract";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <AppProvider>
       <BrowserRouter>
-        <Contract />
+        <App />
       </BrowserRouter>
     </AppProvider>
   </React.StrictMode>
